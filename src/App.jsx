f105beebe7f6ca951a91e20f301e2ae8c3eaf51c
merosman91 <merghanigasimosman@gmail.com>
2025-12-09 19:07:46 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      background: 'orange',
+      color: 'white',
+      fontSize: '24px'
+    }}>
+      🐔 إذا ترى هذا، فالتطبيق يعمل!
+    </div>
+  );
+      }
